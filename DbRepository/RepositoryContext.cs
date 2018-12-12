5e353599359e73b6f640dc3b973bf4e522548baa
Lavsiant <andrey.konkin@tesseris.com>
2018-12-12 13:19:28 +0200

    initoal

---
+++ DbRepository/RepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.UserModel;
+using Model.GuitarTab;
+
+namespace DbRepository 
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Tab> Tabs { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
